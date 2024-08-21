b3ddce58e75754ad5771bf5c37f0354ebc144427
xiyao <48900010+futuretech6@users.noreply.github.com>
2024-08-21 16:10:58 +0800

    Update uncommitted-git.sh

--- uncommitted-git.sh
+++ uncommitted-git.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 BASE_DIR=~
+CURRENT_DIR=$(pwd)
 
 find "$BASE_DIR" -name ".git" | while read gitdir; do
     repo_dir=$(dirname "$gitdir")
@@ -8,3 +9,5 @@ find "$BASE_DIR" -name ".git" | while read gitdir; do
         echo "Repository with uncommitted changes: $repo_dir"
     fi
 done
+
+cd $CURRENT_DIR
