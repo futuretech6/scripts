412c2dc2410547f8181456e95e85ac3a35f1f851
xiyao <48900010+futuretech6@users.noreply.github.com>
2025-05-13 10:47:14 +0800

    Update uncommitted-git.sh

--- uncommitted-git.sh
+++ uncommitted-git.sh
@@ -3,8 +3,7 @@ BASE_DIR=$(pwd)
 
 find "$BASE_DIR" -type d -name ".git" | while read git_dir; do
     repo_dir=$(dirname "$git_dir")
-    cd "$repo_dir"
-    if [[ -n $(git status --porcelain) ]]; then
+    if [[ -n $(git -C $repo_dir status --porcelain) ]]; then
         echo "Repository with uncommitted changes: $repo_dir"
     fi
 done
