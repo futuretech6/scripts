6eccdd323eca6046bb49f8985ec150dcd6c22110
xiyao <futuretech6@foxmail.com>
2025-07-04 13:41:18 +0800

    fix: use http2 for some doh server
    ref: https://datatracker.ietf.org/doc/html/rfc8484#section-5.2

--- doh-speedtest.py
+++ doh-speedtest.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 import base64
 import concurrent.futures
-import http.client
 import struct
 import time
 from typing import Union
-from urllib.parse import urlparse
+
+import httpx  # pip install "httpx[http2]"
 
 timeout = 2
 target_domain = "google.com"
@@ -16,6 +16,7 @@ doh_servers = [
     "https://doh.360.cn/dns-query",
     "https://dns.cloudflare.com/dns-query",
     "https://cloudflare-dns.com/dns-query",
+    "https://dns.google/dns-query",
     "https://dns.quad9.net/dns-query",
     "https://149.112.112.112/dns-query",  # Quad9
     "https://dns.twnic.tw/dns-query",  # Quad101
@@ -54,57 +55,50 @@ def build_wire(domain: str) -> bytes:
 
 def test_doh_server_wire(url: str) -> Union[float, str]:
     try:
-        parsed_url = urlparse(url)
-        conn = http.client.HTTPSConnection(parsed_url.netloc, timeout=timeout)
         dns_query = build_wire(target_domain)
         dns_query_b64 = base64.urlsafe_b64encode(dns_query).rstrip(b"=").decode()
-        path = f"{parsed_url.path}?dns={dns_query_b64}"
         headers = {"Accept": "application/dns-message"}
         start_time = time.time()
-        conn.request("GET", path, headers=headers)
-        response = conn.getresponse()
-        if response.status == 200:
-            return time.time() - start_time
-        else:
-            raise http.client.HTTPException(f"StatusError {response.status}")
+        with httpx.Client(http2=True, timeout=timeout) as client:
+            response = client.get(url, params={"dns": dns_query_b64}, headers=headers)
+            response.raise_for_status()  # Raise an exception for bad status codes (4xx or 5xx)
+        return time.time() - start_time
+    except httpx.HTTPStatusError as e:
+        return f"HTTPStatusError: {e.response.status_code} - {e.response.text.strip()}"
     except Exception as e:
         return f"{type(e).__name__}: {e}"
 
 
 def test_doh_server_wire_post(url: str) -> Union[float, str]:
     try:
-        parsed_url = urlparse(url)
-        conn = http.client.HTTPSConnection(parsed_url.netloc, timeout=timeout)
         dns_query = build_wire(target_domain)
         headers = {
             "Content-Type": "application/dns-message",
             "Accept": "application/dns-message",
         }
         start_time = time.time()
-        conn.request("POST", parsed_url.path, body=dns_query, headers=headers)
-        response = conn.getresponse()
-        if response.status == 200:
-            return time.time() - start_time
-        else:
-            raise http.client.HTTPException(f"StatusError {response.status}")
+        with httpx.Client(http2=True, timeout=timeout) as client:
+            response = client.post(url, content=dns_query, headers=headers)
+            response.raise_for_status()  # Raise an exception for bad status codes (4xx or 5xx)
+        return time.time() - start_time
+    except httpx.HTTPStatusError as e:
+        return f"HTTPStatusError: {e.response.status_code} - {e.response.text.strip()}"
     except Exception as e:
         return f"{type(e).__name__}: {e}"
 
 
 def test_doh_server_json(url: str) -> Union[float, str]:
     try:
-        parsed_url = urlparse(url)
-        conn = http.client.HTTPSConnection(parsed_url.netloc, timeout=timeout)
-        # 构造JSON API参数
-        path = f"{parsed_url.path}?name={target_domain}&type=A"
         headers = {"Accept": "application/dns-json"}
         start_time = time.time()
-        conn.request("GET", path, headers=headers)
-        response = conn.getresponse()
-        if response.status == 200:
-            return time.time() - start_time
-        else:
-            raise http.client.HTTPException(f"StatusError {response.status}")
+        with httpx.Client(http2=True, timeout=timeout) as client:
+            response = client.get(
+                url, params={"name": target_domain, "type": "A"}, headers=headers
+            )
+            response.raise_for_status()  # Raise an exception for bad status codes (4xx or 5xx)
+        return time.time() - start_time
+    except httpx.HTTPStatusError as e:
+        return f"HTTPStatusError: {e.response.status_code} - {e.response.text.strip()}"
     except Exception as e:
         return f"{type(e).__name__}: {e}"
 
@@ -117,7 +111,11 @@ def test_doh_server(url: str) -> Union[float, str]:
     elif isinstance(json_result := test_doh_server_json(url), float):
         return json_result
     else:
-        return f"WIRE {wire_result}; WIRE POST {wire_post_result}; JSON {json_result}."
+        return (
+            f"(WIRE GET ) {wire_result};\n"
+            f"{' ' * (pad_len + len('[ ] : '))}(WIRE POST) {wire_post_result};\n"
+            f"{' ' * (pad_len + len('[ ] : '))}(JSON API ) {json_result}."
+        )
 
 
 print("DoH Server Connectivity Results (wire format):")
