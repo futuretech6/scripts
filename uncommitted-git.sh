94a3bd767a2feb7577189fa18176d13f2fbc15bc
xiyao <futuretech6@foxmail.com>
2026-02-11 16:20:35 +0800

    update uncommitted-git as function

--- uncommitted-git.sh
+++ uncommitted-git.sh
@@ -1,11 +1,9 @@
-#!/bin/bash
-BASE_DIR=$(pwd)
-
-find "$BASE_DIR" -type d -name ".git" | while read git_dir; do
-    repo_dir=$(dirname "$git_dir")
-    if [[ -n $(git -C "$repo_dir" status --porcelain) ]]; then
-        echo "Repository with uncommitted changes: $repo_dir"
-    fi
-done
-
-cd $BASE_DIR
+uncommitted-git() {
+    local base_dir="${1:-$(pwd)}"
+    find "$base_dir" -type d -name ".git" | while read git_dir; do
+        repo_dir=$(dirname "$git_dir")
+        if [[ -n $(git -C "$repo_dir" status --porcelain) ]]; then
+            echo "Repository with uncommitted changes: $repo_dir"
+        fi
+    done
+}
