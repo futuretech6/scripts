06e48e464dedf33a0847041cfb0b06aa5788ef2a
xiyao <48900010+futuretech6@users.noreply.github.com>
2026-07-13 13:59:26 +0800

    Update less command options in print-csv.sh

--- print-csv.sh
+++ print-csv.sh
@@ -3,9 +3,13 @@ pc() {
     local lines="$2"
     local cat_cmd="cat"
     [[ "$file" == *.gz ]] && cat_cmd="zcat"
+    local less_opts="-#2 -N -S"
+    if less --help 2>&1 | grep -q '\-\-header'; then
+        less_opts="$less_opts --header 1"
+    fi
     if [ -n "$lines" ]; then
-        $cat_cmd "$file" | head -n "$lines" | column -s, -t | less -#2 -N -S
+        $cat_cmd "$file" | head -n "$lines" | column -s, -t | less $less_opts
     else
-        $cat_cmd "$file" | column -s, -t | less -#2 -N -S
+        $cat_cmd "$file" | column -s, -t | less $less_opts
     fi
 }
