9ef55fecfbf2e9fcf1027dda6ab0b5091a4d75a7
xiyao <futuretech6@foxmail.com>
2026-07-31 18:53:20 +0800

    add fzf-checkout.sh

---
+++ fzf-checkout.sh
@@ -0,0 +1,12 @@
+# fzf 交互式 git checkout：本地 + 远端分支，按最近提交排序，右侧预览提交历史
+gco() {
+    local branch
+    branch=$(
+        git branch --all --sort=-committerdate \
+            --format='%(refname:short)' 2>/dev/null \
+            | grep -v '^origin/HEAD' \
+            | fzf --ansi --no-multi \
+                  --preview 'git log --oneline --graph --decorate --color=always {} | head -200'
+    ) || return
+    [ -n "$branch" ] && git checkout "${branch#origin/}"
+}
